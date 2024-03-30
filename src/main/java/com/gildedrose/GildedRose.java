902e9c91edce6ac34798ac097be664f52129379e
AndreaMendez09 <andrea.mendez.sanz01@gmail.com>
2024-03-30 17:18:22 +0100

    [refactor] Chango to switch and add methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,56 +3,76 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(TypesOfItemEnum.BRIE.name)
-                    && !item.name.equals(TypesOfItemEnum.CONCERT.name)) {
-                if (item.quality > 0 && (!item.name.equals(TypesOfItemEnum.SULFURAS.name))) {
-                    item.quality = item.quality - 1;
-
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(TypesOfItemEnum.CONCERT.name)) {
-                        if (item.sellIn < 11 && (item.quality < 50)) {
-                            item.quality = item.quality + 1;
-
-                        }
-
-                        if (item.sellIn < 6 && (item.quality < 50)) {
-                            item.quality = item.quality + 1;
-
-                        }
+            switch (item.name) {
+                case "Aged Brie":
+                    decreaseSellIn(item);
+                    calculateQualityAgedBrie(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    decreaseSellIn(item);
+                    calculateQualityBackstage(item);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    decreaseSellIn(item);
+                    decreaseQuality(item);
+                    if (item.sellIn < 0 && item.quality > 0) {
+                        decreaseQuality(item);
                     }
-                }
+                    break;
             }
+        }
+    }
 
-            if (!item.name.equals(TypesOfItemEnum.SULFURAS.name)) {
-                item.sellIn = item.sellIn - 1;
-            }
+    private void calculateQualityAgedBrie(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+        }
+        if (item.quality < 50 && item.sellIn < 0) {
+            increaseQuality(item);
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(TypesOfItemEnum.BRIE.name)) {
-                    if (!item.name.equals(TypesOfItemEnum.CONCERT.name)) {
-                        if (item.quality > 0 && (!item.name.equals(TypesOfItemEnum.SULFURAS.name))) {
-                            item.quality = item.quality - 1;
+    private void calculateQualityBackstage(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 10 && (item.quality < 50)) {
+            increaseQuality(item);
+        }
 
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+        if (item.sellIn < 5 && (item.quality < 50)) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn -= 1;
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality += 1;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > MIN_QUALITY) {
+            item.quality -= 1;
         }
     }
 }
